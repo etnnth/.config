2be3178b6d873e1651fc6d3e87140cf42baab890
etienne <etnnth@gmail.com>
2023-01-12 05:42:43 +0100

    remove luasnip

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -66,17 +66,10 @@ require("lspconfig").rust_analyzer.setup(config({
     }))
 
 
--- luasnip setup
-local luasnip = require 'luasnip'
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -93,7 +86,6 @@ cmp.setup {
   },
   sources = {
     { name = 'nvim_lsp' },
-    { name = 'luasnip' },
     { name = 'nvim_lsp_signature_help' },
   },
 }
