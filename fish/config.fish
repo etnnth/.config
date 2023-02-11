7ee777d5d1629b7236901c9856a3259b74ff7ccc
etienne <etnnth@gmail.com>
2023-02-11 11:12:06 +0100

    remove unused alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,4 @@
 alias watchPerf='watch -n1 "sensors && cat /proc/cpuinfo | grep \"^[c]pu MHz\""'
-alias cfgt='/usr/bin/git --git-dir=$HOME/.cfgt/ --work-tree=$HOME'
-alias retab='nvim -s <(echo -e "gg=G\n:retab\nZZ")'
 alias docker=podman
 
 export COMPOSE_DOCKER_CLI_BUILD=1 
